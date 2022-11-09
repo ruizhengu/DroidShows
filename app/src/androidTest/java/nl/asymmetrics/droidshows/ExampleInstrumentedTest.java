753179579ed9f321796bf31bb0e44e3c3712ab02
guruizhen <ravenna1031@gmail.com>
2022-11-09 15:16:08 +0000

    add jacoco

--- app/src/androidTest/java/nl/asymmetrics/droidshows/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/nl/asymmetrics/droidshows/ExampleInstrumentedTest.java
@@ -1,14 +1,24 @@
 package nl.asymmetrics.droidshows;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
 import android.content.Context;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+
+import nl.asymmetrics.droidshows.ui.AddSerie;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,10 +27,20 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule<AddSerie> activityScenarioRule = new ActivityScenarioRule<>(AddSerie.class);
+
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("nl.asymmetrics.droidshows", appContext.getPackageName());
+    }
+
+    @Test
+    public void testTest() {
+        onView(withId(R.id.add_serie_title)).perform(click());
     }
 }
