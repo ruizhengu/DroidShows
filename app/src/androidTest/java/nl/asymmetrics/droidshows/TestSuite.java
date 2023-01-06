62ba6466c1f345850072b57a4995a421356c485e
guruizhen <ravenna1031@gmail.com>
2023-01-06 14:00:52 +0000

    add app description

--- app/src/androidTest/java/nl/asymmetrics/droidshows/TestSuite.java
+++ app/src/androidTest/java/nl/asymmetrics/droidshows/TestSuite.java
@@ -13,7 +13,11 @@ public class TestSuite {
     @Rule
     public ActivityScenarioRule<DroidShows> activityScenarioRule = new ActivityScenarioRule<>(DroidShows.class);
 
+
     /**
+     * The application can help you manage TV shows and keep track of the episodes that you have or have not seen.
+     * You can also add the air dates of episodes in the calendar.
+     *
      * Before the development:
      * Please make sure the emulator is connected to the Internet.
      * Please login your Google account on the emulator to make sure you can access the Calendar app.
